4be0732998a64b452dd2f0b313f9ab0a4a92bda8
Jelle <jmanzanohaagsma@gmail.com>
2022-10-17 16:51:17 +0200

    finished iteration 1 as driver

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
 // Iteration #1
+const dronesArr = require("./drone.seed.json")
+
+require("../db/index.js")
+
+const Drone = require("../models/Drone.model.js");
+const { default: mongoose } = require("mongoose");
+
+Drone.insertMany(dronesArr)
+.then(() => {
+    console.log("drones añadidos");
+})
+.catch((err) => {
+    console.log(err);
+})
+
+mongoose.connection.close(function () {
+    console.log('Mongoose default connection closed');
+  });
