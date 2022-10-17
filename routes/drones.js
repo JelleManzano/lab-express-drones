fc3bd85cf40e0e510b423efcd60f4c98515f3c1f
Jelle <jmanzanohaagsma@gmail.com>
2022-10-17 17:58:48 +0200

    Finished iteration 3 using control remote for David Lázaro

--- routes/drones.js
+++ routes/drones.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { format } = require("morgan");
 const router = express.Router();
 const Drone = require("../models/Drone.model.js");
 
@@ -17,7 +18,7 @@ router.get("/", (req, res, next) => {
 
 router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  res.render("drones/create-form.hbs")
+  res.render("drones/create-form.hbs");
 });
 
 router.post("/drones/create", (req, res, next) => {
@@ -25,26 +26,49 @@ router.post("/drones/create", (req, res, next) => {
   const droneToAdd = {
     name: req.body.name,
     propellers: req.body.propellers,
-    maxSpeed: req.body.maxSpeed
-  }
+    maxSpeed: req.body.maxSpeed,
+  };
   Drone.create(droneToAdd)
+    .then((response) => {
+      console.log("dron añadido");
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/drones/:id/edit", (req, res, next) => {
+  // Iteration #4: Update the drone
+  let { id } = req.params;
+
+  Drone.findById(id)
+    .then((response) => {
+      res.render("drones/update-form.hbs", {
+        droneUpdate: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post("/drones/:id/edit", (req, res, next) => {
+  // Iteration #4: Update the drone
+  const {id} = req.params
+  const {name, propellers, maxSpeed} =req.body
+
+  Drone.findByIdAndUpdate(id, {
+    name,
+    propellers,
+    maxSpeed
+  })
   .then((response) => {
-    console.log("dron añadido");
     res.redirect("/drones")
   })
   .catch((err) => {
     next(err)
   })
-});
-
-router.get("/drones/:id/edit", (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post("/drones/:id/edit", (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
 });
 
 router.post("/drones/:id/delete", (req, res, next) => {
